4e35ace8f365136a069c1c5a5e98d93c26e5581f
chansereipanha <160982152+chansereipanha@users.noreply.github.com>
2024-04-19 16:57:25 +1200

    Pong

--- pong.py
+++ pong.py
@@ -1,6 +1,6 @@
 import turtle as t
 
-
+#Create our screen interface
 screen = t.Screen()
 width, height = 1000 ,900
 screen.title("PingPong Game")
@@ -11,8 +11,7 @@ score_a = 0
 score_b =  0
 max_score = 5
 
-#Create left paddle
-
+#Create a left paddle
 left_paddle = t.Turtle()
 left_paddle.color("white")
 left_paddle.shape("square")
@@ -21,7 +20,6 @@ left_paddle.penup()
 left_paddle.goto(-480,0)
 
 #Create a right paddle
-
 right_paddle = t.Turtle()
 right_paddle.color("white")
 right_paddle.shape("square")
@@ -29,7 +27,7 @@ right_paddle.shapesize(stretch_wid=5, stretch_len=0.5)
 right_paddle.penup()
 right_paddle.goto(480,0)
 
-
+#Create our ping pong ball
 ball = t.Turtle()
 ball.speed(1)
 ball.shape("circle")
@@ -41,7 +39,7 @@ ball.dx = 0.1
 ball.dy = 0.1
 
 
-
+#Create our scoreboard to keep track of the players score
 score_board = t.Turtle()
 score_board.penup()
 score_board.color("white")
@@ -49,41 +47,47 @@ score_board.goto(0, 400)
 score_board.write(f"Player A: {score_a}              Player B: {score_b}", align="center", font=("Roboto", 15, "bold"))
 score_board.hideturtle()
 
+#Create a function so that the left paddle can move up
 def left_paddle_up():
     y_up = left_paddle.ycor()
     y_up += 30
     left_paddle.sety(y_up)
 
+#Create a function so that the left paddle can move down
 def left_paddle_down():
     y_up = left_paddle.ycor()
     y_up -= 30
     left_paddle.sety(y_up)
 
+#Create a function so that the right paddle can move up
 def right_paddle_up():
     y_up = right_paddle.ycor()
     y_up += 30
     right_paddle.sety(y_up)
 
+#Create a function so that the right paddle can move down
 def right_paddle_down():
     y_up = right_paddle.ycor()
     y_up -= 30
     right_paddle.sety(y_up)
 
-
-screen.listen()
-screen.onkeypress(left_paddle_up, "w")
+#These codes will let you interact with the screen. 
+screen.listen()   #listen() method allows you to capture any input given from the user onto the screen
+screen.onkeypress(left_paddle_up, "w")  #onkeypress() method let's the user designate a key(from keyboard) as a command for a action
 screen.onkeypress(left_paddle_down, "s")
 screen.onkeypress(right_paddle_up, "Up")
 screen.onkeypress(right_paddle_down, "Down")
 
 
-
+#A while true loop so that the code in it will keep running until the end of the game
 while True:
     screen.update()
 
+    #These pair of codes will allow your ball to move and the speed at how fast they'll move
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
+    #These if statements will decide how the ball will interact onec they made impact with the edge of the screen 
     if ball.ycor() > 450:
         ball.sety(450)
         ball.dy *= -1
@@ -92,6 +96,7 @@ while True:
         ball.sety(-450)
         ball.dy *= -1
 
+    #This if statement decide that if the ball were to touch the 
     if ball.xcor() > 500:
         ball.goto(0, 0)
         ball.dx *= -1
